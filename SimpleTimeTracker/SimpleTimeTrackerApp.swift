edd9cafac96c4f918034d32a6a2ee88bae5d2b6c
Tim Hradil <timhradil@gmail.com>
2023-03-16 14:05:03 -0700

    Initial Commit

---
+++ SimpleTimeTracker/SimpleTimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTimeTrackerApp.swift
+//  SimpleTimeTracker
+//
+//  Created by Tim Hradil on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
